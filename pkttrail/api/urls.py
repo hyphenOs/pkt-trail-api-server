a0e059b6e72018c5f7778d3934d28bbaa961dfae
Abhijit Gadgil <gabhijit@hyphenos.io>
2020-09-11 14:33:15 +0530

    Moved the individual packates into namespace package
    pkt_trail_api is now pkttrail.api package - Similarly we'll have -
    
    pkttrail.schema - Schema for API to Agent communication
    pkttrail.agent - Code and other things

--- pkttrail/api/urls.py
+++ pkttrail/api/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("users/", include('pkt_trail_api.apps.users.urls')),
-    path("resources/", include('pkt_trail_api.apps.resources.urls')),
+    path("users/", include('pkttrail.api.apps.users.urls')),
+    path("resources/", include('pkttrail.api.apps.resources.urls')),
 ]
