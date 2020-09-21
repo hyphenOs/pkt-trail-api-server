fbdc3f90501844f45538d0aee20c5167daa1a78d
Abhijit Gadgil <gabhijit@hyphenos.io>
2020-09-21 14:37:15 +0530

    Basic support for 'init' and 'keepalive' message in `agents` App
    A basic support for 'init' and 'keepalive' messages is in place now. We
    are validating Request Schema and returning 400 errors if the schema is
    not valid. Otherwise a Success is returned. We are not yet generating
    the proper `*Response` messages.

--- pkttrail/api/apps/agents/views.py
+++ pkttrail/api/apps/agents/views.py
@@ -3,11 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 from rest_framework import viewsets
+from rest_framework import status as http_status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
 
-from pkttrail.schema.messages import PktTrailInitRequestSchema
+from pkttrail.schema.messages import (
+        PktTrailSchemaValidationError,
+        PktTrailInitRequestSchema,
+        PktTrailKeepAliveRequestSchema
+    )
 
 
 class PktTrailAgentsViewSet(viewsets.ViewSet):
@@ -15,9 +20,28 @@ class PktTrailAgentsViewSet(viewsets.ViewSet):
 
     @action(detail=False, methods=['post'])
     def init(self, request):
-        return Response(dict(message="ok"))
+        """Handler for `init` request from the agent."""
+
+        try:
+            init_req = PktTrailInitRequestSchema().loads(
+                    request.body.decode("utf-8"))
+
+            return Response(dict(message="ok"))
+
+        except PktTrailSchemaValidationError:
+            return Response(dict(message="Bad Request"),
+                    status=http_status.HTTP_400_BAD_REQUEST)
 
 
     @action(detail=False, methods=['post'])
     def keepalive(self, request):
-        return Response(dict(message="ok"))
+        """Handler for `keep-alive` request from the agent."""
+        try:
+            keep_alive_req = PktTrailKeepAliveRequestSchema().loads(
+                    request.body.decode("utf-8"))
+
+            return Response(dict(message="ok"))
+
+        except PktTrailSchemaValidationError:
+            return Response(dict(message="Bad Request"),
+                    status=http_status.HTTP_400_BAD_REQUEST)
